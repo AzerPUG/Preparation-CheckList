9627c2501ad7bed16569f42a9756972f7b06ab3a
Arco Dielhof <arcodielhof@hotmail.com>
2020-11-17 21:42:48 +0100

    AutoUpload test 1.

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AIU = ...
 
-local AZPIUCheckListVersion = 16
+local AZPIUCheckListVersion = 17
 local dash = " - "
 local name = "InstanceUtility" .. dash .. "CheckList"
 local nameFull = ("AzerPUG " .. name)
