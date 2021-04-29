3bf6ff3c6c6801da06d22a9544030bce9235310b
Arco Dielhof <arcodielhof@hotmail.com>
2021-04-29 15:40:29 +0200

    Version++;

--- PreparationCheckList.lua
+++ PreparationCheckList.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Preparation CheckList"] = 25
+AZP.VersionControl["Preparation CheckList"] = 26
 if AZP.PreparationCheckList == nil then AZP.PreparationCheckList = {} end
 
 local itemCheckListFrame
