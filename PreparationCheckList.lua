8585c4aa94623608419640234ec09ac7fa071cb6
Arco Dielhof <arcodielhof@hotmail.com>
2022-03-01 11:42:27 +0100

    Update for patch 9.2 to no longer show as out of date!

--- PreparationCheckList.lua
+++ PreparationCheckList.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Preparation CheckList"] = 33
+AZP.VersionControl["Preparation CheckList"] = 34
 if AZP.PreparationCheckList == nil then AZP.PreparationCheckList = {} end
 if AZP.PreparationCheckList.Events == nil then AZP.PreparationCheckList.Events = {} end
 
